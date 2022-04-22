50e7a1adb0a6eb0e66382df3c5a26a29362c80a3
Matheus Felipe Rodrigues <88681715+Matheyck@users.noreply.github.com>
2022-04-22 15:38:56 -0300

    Condicional

---
+++ Condicional.py
@@ -0,0 +1,9 @@
+idade = 16
+
+def verifica_se_pode_dirigir(idade):
+  if idade >= 18:
+    print('Tem permissão para dirigir')
+  else:
+    print('Não tem permissão para dirigir')
+
+verifica_se_pode_dirigir(idade)
