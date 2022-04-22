12cadd57754ab17be330130ca20ee46c4f31b0b4
Matheus Felipe Rodrigues <88681715+Matheyck@users.noreply.github.com>
2022-04-22 15:45:51 -0300

    Tipos de uma lista

---
+++ Type.py
@@ -0,0 +1,9 @@
+lista = ['Matheus', 28, True, '18']
+
+for elemento in lista:
+  print(f'O elemento {elemento} é do tipo: ',type(elemento))
+  
+# O elemento Matheus é do tipo:  <class 'str'>
+# O elemento 28 é do tipo:  <class 'int'>
+# O elemento True é do tipo:  <class 'bool'>
+# O elemento 18 é do tipo:  <class 'str'>
